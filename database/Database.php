685c898ec25e0bb0ea607fb6dbdbe35886fe3183
Zainab Saad <105385638+Zainab-Saad@users.noreply.github.com>
2023-12-04 09:43:14 +0500

    Add content search functionality (#1)
    - This content search module uses the postgres full-text
      search to find for the query strings in the stored content
      corresponding each URL
    
    - This PR also seperates the interface for crawling and web search

--- database/Database.php
+++ database/Database.php
@@ -11,6 +11,7 @@
     public function create_table() {
       $sql =  "CREATE TABLE IF NOT EXISTS crawl_result (".
               "url TEXT PRIMARY KEY,".
+              "title TEXT,".
               "content TEXT,".
               "search_content tsvector GENERATED ALWAYS AS (to_tsvector('english', content)) STORED,".
               "updated_on TIMESTAMP".
@@ -19,15 +20,16 @@
       $this->pdo->exec($sql);
     }
 
-    public function insert_row($url, $content) {
-      $sql = "INSERT INTO crawl_result (url, content, updated_on) VALUES ".
-            "(:url, :content, :updated_on)";
+    public function insert_row($url, $content, $title) {
+      $sql = "INSERT INTO crawl_result (url, title, content, updated_on) VALUES ".
+            "(:url, :title, :content, :updated_on)";
 
       $stmt = $this->pdo->prepare($sql);
 
       $updated_on = date('Y-m-d H:i:s');
 
       $stmt->bindValue(':url', $url);
+      $stmt->bindValue(':title', $title);
       $stmt->bindValue(':content', $content);
       $stmt->bindValue(':updated_on', $updated_on);
 
@@ -51,6 +53,25 @@
           ];
       }
       return count($results);
+    }
+
+    public function search_content($search_string) {
+      $sql = "SELECT url, title, content FROM crawl_result ".
+              "WHERE search_content @@ to_tsquery('english', '".$search_string."')";
+      
+      $stmt = $this->pdo->query($sql);
+
+      $results = [];
+
+      while($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+        $results[] = [
+          'url' => $row['url'],
+          'title' => $row['title'],
+          'content' => $row['content']
+        ];
+      }
+
+      return $results;
     }
   }
 ?>
--- index.php
+++ index.php
@@ -4,15 +4,21 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Web Search</title>
-  <link rel="stylesheet" href="./public/css/main.css">
 </head>
+<style>
+  <?php include __DIR__."\public\css\main.css"; ?>
+</style>
 <body>
-  <h2>Web Search</h2>
+  <button class="btn-square">
+    <a href=".\src\content_search.php">Go To Search Engine</a>
+  </button>
+  <h2>Web Crawler</h2>
   <form action="#" method="POST">
-    <label for="url">Enter a seed url</label>
+    <label for="url" class="form-label">Enter a seed url</label>
     <input type="text" placeholder="URL e.g; www.google.com" name="url" class="input-field">
-    <button type="submit" class="btn">Enter</button>
+    <button type="submit" class="btn">Start Crawling</button>
   </form>
+
 </body>
 <?php
   require_once __DIR__."\src\logger.php";
